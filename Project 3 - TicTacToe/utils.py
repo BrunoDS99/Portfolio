d106b9350bf6fb82fbb22cc14eaa5dc33f3e93ae
BrunoDS99 <brunodantas99@gmail.com>
2026-07-30 00:52:39 +0100

    Made changed to stats filepath. It now saved into project folder instead of IDE workspace

--- Project 3 - TicTacToe/utils.py
+++ Project 3 - TicTacToe/utils.py
@@ -2,8 +2,16 @@ import json
 import os
 from models import GameStats
 
+
+def get_stats_filepath(filename: str = 'stats.json') -> str:
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    return os.path.join(current_dir, filename)
+
+
 def save_stats(stats: GameStats, filename: str = 'stats.json') -> None:
     """Save statistics to file"""
+    
+    filepath = get_stats_filepath()
     
     data = {
         "total_games": stats.total_games,
@@ -12,18 +20,22 @@ def save_stats(stats: GameStats, filename: str = 'stats.json') -> None:
         "draws": stats.draws
     }
 
-    with open(filename, 'w') as f:
+    with open(filepath, 'w') as f:
         json.dump(data, f)
+    print(f"Stats saved to: {filepath}")
 
 def load_stats(filename: str = 'stats.json') -> GameStats:
     """Load statistics from file."""
-    if os.path.exists(filename):
-        with open(filename, 'r') as f:
+    
+    filepath = get_stats_filepath(filename)
+    if os.path.exists(filepath):
+        with open(filepath, 'r') as f:
             data = json.load(f)
             stats = GameStats()
             stats.total_games = data.get("total_games", 0)
             stats.x_wins = data.get("x_wins", 0)
             stats.o_wins = data.get("o_wins", 0)
             stats.draws = data.get("draws", 0)
+            print(f"Stats loaded from: {filepath}")
             return stats
     return GameStats()
